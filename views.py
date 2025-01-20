9c2b61df0234af9dd18c746c78dbf8df440b8a46
Satoshi Samourai <satoshi_samourai@protonmail.com>
2025-01-20 20:57:02 +0100

    fix: update template structure to match LNbits standards

--- views.py
+++ views.py
@@ -13,7 +13,7 @@ boltt_generic_router = APIRouter()
 
 
 def boltt_renderer():
-    return template_renderer(["templates"])
+    return template_renderer(["boltt/templates"])
 
 
 @boltt_generic_router.get("/", response_class=HTMLResponse)
